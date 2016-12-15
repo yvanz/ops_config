56e032b235739fb8129390bfe9532eb9623ff119
Yvan <yvan.zy@gmail.com>
2016-12-15 14:31:39 +0800

    修改发现进程的写法

--- zabbix/app_discovery.py
+++ zabbix/app_discovery.py
@@ -5,20 +5,19 @@ import time
 
 def main():
 	#提取程序执行命令
-	str_app_name = subprocess.check_output("ps aux | grep case | grep -v rsync | grep -v grep | awk '{print $NF}'",shell=True)
+	str_app_name = subprocess.check_output("ps U www | grep case | awk '{print $NF}'",shell=True)
 	list_app_name = [ i for i in str_app_name.split('\n') if i != '' ]
 
 	print '{'
 	print '    "data": ['
 
-	#提取程序PID以及程序名称
+	#提取程序名称
 	for app in list_app_name:
 		if app != list_app_name[-1]:
 			list_index = 0
 		else:
 			list_index = 1
 		app_name = app.split('/')[-1]
-		app_pid = [ i for i in subprocess.check_output(['/usr/bin/pgrep', '-f' ,app]).split('\n')][0]
 		print '        {'
 		print '            "{#APPNAME}": "%s"' %(app_name)
 		if list_index != 1:
