79ce8b9e4bb57583ce0413f04ed7348c69a46d50
Yvan <yvan.zy@gmail.com>
2016-12-16 18:23:36 +0800

    使用json标准库显示发现的进程

--- zabbix/app_discovery.py
+++ zabbix/app_discovery.py
@@ -1,32 +1,25 @@
-#!/usr/bin/env python27
+#!/usr/bin/env python
 #coding=utf-8
 import subprocess
-import time
+import json
 
 def main():
-	#提取程序执行命令
-	str_app_name = subprocess.check_output("ps U www | grep case | awk '{print $NF}'",shell=True)
-	list_app_name = [ i for i in str_app_name.split('\n') if i != '' ]
+        #提取程序执行命令
+        str_app_name = subprocess.check_output("ps U www | grep case | awk '{print $NF}'",shell=True)
+        list_app_name = [ i for i in str_app_name.split('\n') if i != '' ]
 
-	print '{'
-	print '    "data": ['
+        l = []
+        app_json = dict()
 
-	#提取程序名称
-	for app in list_app_name:
-		if app != list_app_name[-1]:
-			list_index = 0
-		else:
-			list_index = 1
-		app_name = app.split('/')[-1]
-		print '        {'
-		print '            "{#APPNAME}": "%s"' %(app_name)
-		if list_index != 1:
-			print '        },'
-		else:
-			print '        }'
+        #提取程序名称
+        for app in list_app_name:
+                temp_dict = dict()
+                app_name = app.split('/')[-1]
+                temp_dict["{#APPNAME}"]=app_name
+                l.append(temp_dict)
 
-	print '    ]'
-	print '}'
+        app_json["data"] = l
+        print json.dumps(app_json)
 
 if __name__ == '__main__':
-	main()
+        main()
